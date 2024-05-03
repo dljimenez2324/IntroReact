43c80c41df41c815ec53e70fd2cb448e81e9dfee
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-02 20:01:45 -0700

    first commit

---
+++ Intro_React_App/src/App.tsx
@@ -0,0 +1,15 @@
+import Message from "./Message";
+
+
+function App()
+{
+  const name = "David";
+  //return <div><Message/></div>
+  if (name)
+  {
+    
+    return <div>Hello{name}</div>
+    return <div><h1>Hello World</h1></div>
+  }
+}
+export default App;
---
+++ Intro_React_App/src/Message.tsx
@@ -0,0 +1,23 @@
+//Functional components
+//PascalCasing
+function Message()
+{
+    //JSX: Javascript & XML combination
+    return <h1>Hello World</h1>
+}
+
+// in order to use this, we need to export this function
+// this export will allow us to use this component where ever we import it
+export default Message;
+
+// //Functional components
+// //PascalCasing
+// function Message()
+// {
+//     //JSX: Javascript & XML combination
+//     return <h1>Hello World from my message</h1>
+// }
+
+// // in order to use this, we need to export this function
+// // this export will allow us to use this component where ever we import it
+// export default Message;
