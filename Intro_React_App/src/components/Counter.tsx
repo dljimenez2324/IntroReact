5d6e72459309c9d89b6010cbf71b582cc29591f1
dljimenez2324 <dljimenez2324@gmail.com>
2024-05-08 20:46:00 -0700

    added and updated the counter.tsx

--- Intro_React_App/src/components/Counter.tsx
+++ Intro_React_App/src/components/Counter.tsx
@@ -6,22 +6,30 @@ const Counter = () => {
     // declare and initialize a useState to hold a number that we will increment and decrement
 
     const [numbers, setNumbers] = useState(0);
+
     const incrementNumbers = () => {
-        setNumbers(numbers + 1);
-        console.log(`The counter is at # ${numbers}`)
+        setNumbers((prevNumbers) => prevNumbers + 1);
+        //console.log(`The counter is at # ${numbers}`)
     }
+
     const decrementNumbers = () => {
        
-        if (numbers >=0) {
+        if (numbers > 0) {
             
-            setNumbers(numbers - 1);
-            console.log(`The counter is at # ${numbers}`)
+            setNumbers((prevNumbers) => prevNumbers - 1);
+            //console.log(`The counter is at # ${numbers}`)
         }
         else {
-            console.log(`The counter is not allowed to be negative`)
-            setNumbers(0);
+            //console.log(`The counter is not allowed to be negative`)
+            return alert("The counter is not allowed to be negative");
+            //setNumbers(0);
+            // insde the setNumbers((prevNumbers) => prevNumbers + 1)
             // FELL ASLEEP AT DESK HERE
         }
+    }
+
+    const resetNumbers = () => {
+        setNumbers(0);
     }
 
   return (
@@ -30,6 +38,7 @@ const Counter = () => {
     <div>Current Number: {numbers}</div>
     <button className="btn btn-success" onClick={incrementNumbers}>+ 1</button>
     <button className="btn btn-danger" onClick={decrementNumbers}>- 1</button>
+    <button className="btn btn-warning" onClick={resetNumbers}>Reset</button>
     </>
   )
 }
